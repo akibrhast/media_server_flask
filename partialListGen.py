ba50fdd9ee618eab001cb3f8ac4ae5dc80724156
akibr <akibr@DESKTOP-J5FBATK>
2019-05-25 00:53:03 -0400

    Create partialListGen.py

---
+++ partialListGen.py
@@ -0,0 +1,11 @@
+import time
+import os
+
+examplie_list = [numbers for numbers in range(0,5000)]
+
+def partialListGen():
+	for i in range(0,len(examplie_list),10):
+		yield examplie_list[i:i+10]
+		
+partialList=partialListGen()
+next(partialList)
